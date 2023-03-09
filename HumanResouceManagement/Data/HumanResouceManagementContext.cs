d61d5fa867c1489d7b95195ae2362ad6db0c3333
Duy Linh <91305016+taduylinh0396077238@users.noreply.github.com>
2023-03-09 11:09:30 +0700

    finish

---
+++ HumanResouceManagement/Data/HumanResouceManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HumanResouceManagement;
+
+namespace HumanResouceManagement.Data
+{
+    public class HumanResouceManagementContext : DbContext
+    {
+        public HumanResouceManagementContext (DbContextOptions<HumanResouceManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HumanResouceManagement.Employees> Employees { get; set; } = default!;
+    }
+}
---
+++ HumanResouceManagement/Employees.cs
@@ -0,0 +1,11 @@
+﻿namespace HumanResouceManagement
+{
+    public class Employees
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+        public int age { get; set; }
+        public string description { get; set; } 
+    }
+}
